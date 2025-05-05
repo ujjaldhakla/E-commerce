a15e1bc61ed8f4a44c1b1175cf87190d1081d6e9
Ujjal Dhakal <udhakal555@gmail.com>
2025-05-05 15:22:10 +0545

    Initial commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const session = require('express-session');
 const db = require('./db');
 require('dotenv').config();
